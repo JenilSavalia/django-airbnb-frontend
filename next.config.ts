75fab95144560be80046031dffb6ca1f72c1d391
Jenil <jenil.savalia.cd@gmail.com>
2026-07-08 15:24:42 +0530

    edited next config

--- next.config.ts
+++ next.config.ts
@@ -4,16 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
 
   images: {
-    dangerouslyAllowLocalIP: process.env.NODE_ENV === 'development',
+    dangerouslyAllowLocalIP: process.env.NODE_ENV === "development",
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '8000',
-        pathname: '/**'
-      }
-    ]
-  }
+        protocol: "http",
+        hostname: "localhost",
+        port: "8000",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "https://django-airbnb-backend.vercel.app", // Replace with your Django backend domain
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
